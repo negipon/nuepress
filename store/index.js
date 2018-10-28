054f62268ee32465e8d2d24e08f7a2d0af9c0682
cmsmix <cmsmix@gmail.com>
2018-10-28 09:05:19 +0900

    up

--- store/index.js
+++ store/index.js
@@ -28,7 +28,7 @@ const store = () => new Vuex.Store({
     },
     topicArticles: [],
     topics: null,
-    wordpressAPI: 'https://wp-api.kmr.io/wp-json'
+    wordpressAPI: 'https://wpnuxt.ipon.biz/api/wp-json'
   },
 
   mutations: {
